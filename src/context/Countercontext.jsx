15ca1d101d9b221043d3f6511ad5d9d1444c897e
momen72 <66283391+momen72@users.noreply.github.com>
2026-05-21 13:09:26 +0000

    new

--- src/context/Countercontext.jsx
+++ src/context/Countercontext.jsx
@@ -1,10 +1,12 @@
 import { useState} from "react"
+import { useEffect } from "react"
 import { Counter } from "./CounterProvider"
 
 // export { Counter }
 export default function Countercontext({children}) {
 
 const[usertoken,setusertoken]=useState(null)
+useEffect(functio)
 
 function settoken(tkn){
     setusertoken(tkn) 
